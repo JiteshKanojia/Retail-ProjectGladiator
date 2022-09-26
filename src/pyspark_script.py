520d492c8a47c3cf8be306c16d2d257890491a60
JiteshKanojia <jiteshkanojia22@gmail.com>
2022-09-26 15:29:57 +0530

    added reading csv from HDFS

--- src/pyspark_script.py
+++ src/pyspark_script.py
@@ -1,10 +1,25 @@
+#Use spark-submit to the the script
+
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
-app_name = "hive_pyspark"
+from pyspark.sql.types import StructType,StructField,IntegerType,StringType
+
+app_name = "Retail"
 master = "local"
 
 spark = SparkSession.builder.master(master).appName(app_name).enableHiveSupport().getOrCreate()
 
+#Read databases present in HIVE.
 df = spark.sql("show databases")
 df.show()
+
+#Read from HDFS
+#Create Schema First
+region_schema = StructType() \
+        .add(StructField("id",IntegerType(),True)) \
+        .add(StructField("name",StringType(),True)) \
+        .add(StructField("comment",StringType(),True))
+#Read csv from HDFS Location
+df = spark.read.csv("hdfs://localhost:9000/user/ak/retail/region.csv", schema=region_schema, sep='\t', header=True)
+df.show(3)
