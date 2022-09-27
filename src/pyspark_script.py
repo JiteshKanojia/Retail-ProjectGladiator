e3c9935ccceabfcc2b585ad0ddca13536bb400fd
JiteshKanojia <jiteshkanojia22@gmail.com>
2022-09-27 14:14:26 +0530

    removed trailing spaces

--- src/pyspark_script.py
+++ src/pyspark_script.py
@@ -97,4 +97,6 @@ total_discounted_extended_taxed.write.format("csv").mode("overwrite").option("he
 
 #Save Transformations to parquet format
 total_extended_price.write.format("parquet").mode("overwrite").option("header","true").save("../outputData/parquet/TotalExtendedPrice")
-total_discounted_extended_price.write.format("parquet").mode("overwrite").option("header","true").save("../outputData/parquet/TotalDiscountedExtendedPrice")  
+total_discounted_extended_price.write.format("parquet").mode("overwrite").option("header","true").save("../outputData/parquet/TotalDiscountedExtendedPrice")
+total_discounted_extended_taxed.write.format("csv").mode("overwrite").option("header","true").save("../outputData/csv/TotalDiscountedExtendedTaxedPrice")
+
